e6554cd47bbc6c2a82840ba6ec21961ca29fa0d5
tapadipti <tapadipti@gmail.com>
2022-11-04 20:36:33 +0545

    Updated train script

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
-import os, json, random
+import os, json, random, yaml
 
-metrics_dict = {"loss": random.random(), "accuracy": random.random()}
+params = yaml.safe_load(open("params.yaml"))["train"]
+accuracy = params["accuracy"]
+
+
+metrics_dict = {"loss": 0.1, "accuracy": random.random()}
 METRICS_FILE = os.path.join("metrics.json")
 with open(METRICS_FILE, "w") as f:
     f.write(json.dumps(metrics_dict))
